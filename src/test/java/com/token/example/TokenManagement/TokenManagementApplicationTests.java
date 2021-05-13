772e19d8e510b22911ade0ba7539e62c6770e74a
Camilo <contreras.camilo32@gmail.com>
2021-05-13 17:38:11 -0400

    initial project loaded

---
+++ src/test/java/com/token/example/TokenManagement/TokenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.token.example.TokenManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
